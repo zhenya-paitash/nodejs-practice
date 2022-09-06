00ccdbefd55129ec097dc2b97d006f2a2867f29f
zhenya-paitash <naitavv93@gmail.com>
2022-09-06 11:46:26 +0300

    Add .env. Refresh token

--- finance-tg-bot/src/app.ts
+++ finance-tg-bot/src/app.ts
@@ -1,12 +1,19 @@
 import { Telegraf } from 'telegraf'
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import { config } from 'dotenv'
 
-const bot = new Telegraf('5681517300:AAFUv8Z44100G8NiQWJxq_TVuLlpB7y-fjA');
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hello'));
-bot.launch();
+config({ path: './.env' })
+const { TG_BOT_TOKEN, GOOGLE_SS_EMAIL, GOOGLE_SS_KEY } = process.env
+
+const bot = new Telegraf(TG_BOT_TOKEN!)
+
+// bot.start((ctx) => ctx.reply('Welcome'))
+// bot.help((ctx) => ctx.reply('Send me a sticker'))
+// bot.on('sticker', (ctx) => ctx.reply('👍'))
+
+bot.hears('hi', (ctx) => ctx.reply('Hello'))
+bot.launch()
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
